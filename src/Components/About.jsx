dcb3d5428a545d2960124601462140d2701f9a8a
sripriya503 <sripriya503chemurgy@gmail.com>
2024-02-13 17:37:38 +0530

    commit4

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CSE IoT student studying at Jawaharlal Nehru Technical University Kakinada (Affiliated). I relish fashioning unique and sophisticated user interfaces creative in Responsive ways";
 
 /**
  * List of some of skills or technologies you work on, are learning,
